159c5e0a479a7ea12cc597fbc60fff45ddeb7309
isabelsaa <isabel89@protonmail.com>
2026-01-28 14:00:36 +0100

    [ADD] - Add first algorithm

--- python/number_theory/sieve_erastothenes.py
+++ python/number_theory/sieve_erastothenes.py
@@ -1,5 +1,12 @@
-def sieve_erastothenes(name):
+def sieve_erastothenes(until_number):
+    numbers = list(range(1, until_number + 1))
+    numbers.remove(1)
+    for firstIndex in numbers[:]:
+        for secondIndex in numbers[:]:
+            if secondIndex != firstIndex and secondIndex % firstIndex == 0:
+                numbers.remove(secondIndex)
+    print(numbers)
 
-    print(f'Hi, {name}')
+
 if __name__ == '__main__':
-    sieve_erastothenes('Erastothenes')
+    sieve_erastothenes(10)
